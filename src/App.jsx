c87ac14606cd31c14021ac3ded94e61c7fa6a971
eligazza <gazzaelias@gmail.com>
2023-08-16 21:51:39 -0300

    subo la resolucion del examen

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,57 @@
+import { useState } from 'react'
 import './App.css'
 import Card from './Components/Card'
 
+
 function App() {
 
+  // Creo un estado con un objeto que guardará los datos
+  const [student, setStudent] = useState({
+    name: '',
+    lastname: '',
+    carreer: ''
+  })
+
+  //Capturo los datos del formulario y los cargo en el objeto anterior (sin espacios)
+  const handleName = (e) => {setStudent({...student, name: e.target.value.trim()})}
+  const handleLastname = (e) => {setStudent({...student, lastname:e.target.value.trim()})}
+  const handleCarreer = (e) => {setStudent({...student, carreer: e.target.value.trim()})}
+
+  // creo las flags para el mensaje de error o de confirmación
+  const [error, setError] = useState(false)
+  const [success, setSuccess] = useState(false)
+  
+  //valido el envío
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (student.name.length < 3 || student.lastname.length < 6 || student.carreer.length < 3) {
+      setError(true);
+      setSuccess(false)
+    }
+    else {
+      setError(false)
+      setSuccess(true)
+    }
+  }
+
   return (
-    <div>
-      <h1>Carga de estudiantes</h1>
-      <form>
-        <label></label>
-        <input type="text"/>
-        <button>Agregar estudiante</button>
+    <div className='container'>
+      
+      <h2>Carga de estudiantes</h2>
+      
+      <form onSubmit={handleSubmit} className='form'>
+        <label>Nombre</label>
+        <input type="text" name='name' value={student.name} onChange={handleName}/>
+        <label>Apellido</label>
+        <input type="text" name='lastname' value={student.lastname} onChange={handleLastname}/>
+        <label>Carrera</label>
+        <input type="text" name='carreer' value={student.carreer} onChange={handleCarreer}/>
+        <button>Agregar</button>
       </form>
-      <Card/>
+      
+      {error && <p className='error'>Por favor chequea que la información sea correcta</p>}
+      {success && <Card student={student}/>}
+    
     </div>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,14 @@
 import style from './Card.module.css'
 
-const Card = () => {
+const Card = ({student}) => {
   return (
-    <div className={style.card}> Hola, soy una tarjeta </div>
+    <div className={style.card}>
+      <h3>¡Cargado con éxito!</h3>
+      <p className={style.strong}>Datos del estudiante</p>
+      <p>Nombre: {student.name}</p>
+      <p>Apellido: {student.lastname}</p>
+      <p>Carrera: {student.carreer}</p>
+    </div>
   )
 }
 
